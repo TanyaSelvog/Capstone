4a572350d6dd5ca898c7f61be48ef083b9294787
Tanya Selvog <tanyaselvog@gmail.com>
2022-12-28 11:05:27 -0800

    working on ClientViewController; editing code in utils and controllers

--- src/controller/ClientViewController.java
+++ src/controller/ClientViewController.java
@@ -1,4 +1,17 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class ClientViewController {
+    public void onAddNewClient(ActionEvent actionEvent) {
+    }
+
+    public void onModClient(ActionEvent actionEvent) {
+    }
+
+    public void onBack(ActionEvent actionEvent) {
+    }
+
+    public void onDelete(ActionEvent actionEvent) {
+    }
 }
